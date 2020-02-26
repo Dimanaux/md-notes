a037da289518b30ee783b57838ea317ba4ceab7d
Dmitry Barskov <barskoidim@gmail.com>
2020-02-26 16:22:52 +0300

    remove full name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :rememberable, :trackable, :validatable, :lockable
-
-  validates :full_name, presence: true
 end
