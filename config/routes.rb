59c68935aba1194b8e05b868f3caa37669016f48
Dmitry Barskov <barskoidim@gmail.com>
2020-02-26 21:41:16 +0300

    Generate model note

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :notes
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
   resources :users, only: %i[show], param: :username
