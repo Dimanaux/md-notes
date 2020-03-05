8bf523934420ef3ee40a1105c31f1b3770662081
Dmitry Barskov <barskoidim@gmail.com>
2020-03-05 07:19:40 +0300

    add spec for user routing

---
+++ spec/routing/users_routing_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe UsersController, type: :routing do
+  describe 'routing' do
+    it 'routes to #show' do
+      expect(get: '/users/user123').to route_to('users#show', username: 'user123')
+    end
+  end
+end
