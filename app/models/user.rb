580ddaa9bad189b31068394723870f4dd2840632
Dmitry Barskov <barskoidim@gmail.com>
2020-03-25 19:50:56 +0300

    fix code style offences

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,30 @@ class User < ApplicationRecord
 
   has_many :notes, dependent: :destroy
 
-  has_and_belongs_to_many(:users,
-    :join_table => "subscriptions",
-    :foreign_key => "follower_id",
-    :association_foreign_key => "subscription_id")
+  has_and_belongs_to_many(
+    :users,
+    join_table: "subscriptions",
+    foreign_key: "follower_id",
+    association_foreign_key: "subscription_id"
+  )
 
   def followers
     User.joins("INNER JOIN subscriptions ON users.id = subscriptions.follower_id")
-        .where("subscriptions.subscription_id = ?", self.id)
+        .where("subscriptions.subscription_id = ?", id)
   end
 
   def subscriptions
     # User.joins("INNER JOIN subscriptions ON users.id = subscriptions.subscription_id")
-        # .where("subscriptions.follower_id = ?", self.id)
+    #     .where("subscriptions.follower_id = ?", id)
     users
   end
 
   def subscribe_to(other_user)
-    self.subscriptions << other_user
+    subscriptions << other_user
   end
 
   def unsubscribe_from(other_user)
-    self.subscriptions.destroy(other_user)
+    subscriptions.destroy(other_user)
   end
 
   def to_param
