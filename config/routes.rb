f04cc1e7a2135beb7accf2ee42699b638acc535a
Dmitry Barskov <barskovdmitry42@gmail.com>
2020-03-27 23:52:22 +0300

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
   resources :users, only: %i[show], constraints: { user_id: /[\w\.-]+/ } do
-    resources :notes, module: :users do
-      resources :ratings, module: :notes, only: %i[create update destroy]
-    end
+    resources :notes, module: :users
   end
   resources :notes, only: %i[index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [], constraints: { user_id: /[\w\.-]+/ } do
+        resources :notes, only: [], module: :users do
+          resources :ratings, module: :notes, only: %i[create update destroy]
+        end
+      end
+    end
+  end
 end
