ea38ec219f9d73c9832fd83befda199f41bbe0ab
Dmitry Barskov <barskoidim@gmail.com>
2020-03-30 22:39:06 +0300

    remove migrations

--- db/migrate/20200324212223_create_subscriptions_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateSubscriptionsJoinTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table "subscriptions", force: true, id: false do |t|
-      t.integer "follower_id", null: false
-      t.integer "subscription_id", null: false
-    end
-  end
-end
--- db/migrate/20200324220719_add_index_on_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexOnSubscriptions < ActiveRecord::Migration[5.1]
-  def change
-    add_index :subscriptions, [:follower_id, :subscription_id], unique: true
-  end
-end
