90aa8903513a6963a8788158b296d95150b845af
Dmitry Barskov <barskoidim@gmail.com>
2020-03-31 22:28:01 +0300

    delegate subscribed_to

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,5 @@
 class UserDecorator < ApplicationDecorator
-  delegate :username, :email
+  delegate :username, :email, :subscribed_to?
 
   def username_with_email
     "#{object.username} (#{object.email})"
