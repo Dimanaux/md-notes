69536a6e77de92b9b11f64b5113ae1dbfdd3d16a
Dmitry Barskov <barskoidim@gmail.com>
2020-03-31 22:27:39 +0300

    fix users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:id])
   end
 
   private
 
   def user_params
-    params.permit(%i[username])
+    params.permit(%i[id])
   end
 end
