466d6b2e6ba82bd3fa811562e13470301aa8c173
Dmitry Barskov <barskoidim@gmail.com>
2020-04-01 11:33:48 +0300

    add spec for mailer

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,7 +1,7 @@
 class SubscriptionMailer < ApplicationMailer
   default from: "md_notes_notifications@example.com"
 
-  def note_notification_email
+  def note_notification_email(params)
     @follower = params[:follower]
     @note = params[:note]
     mail(to: @follower.email, subject: "md_notes notifications")
--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
-describe SubscriptionMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SubscriptionMailer do
+  describe ".note_notification_email" do
+    subject(:mail) { described_class.note_notification_email(note: note, follower: follower) }
+
+    let(:author) { build(:user) }
+    let(:note) { build(:note, user: author) }
+    let(:follower) { build(:user, followees: [author]) }
+
+    it "sets the subject" do
+      expect(mail.subject).to eql("md_notes notifications")
+    end
+
+    it "sends email to follower" do
+      expect(mail.to).to eq [follower.email]
+    end
+  end
 end
