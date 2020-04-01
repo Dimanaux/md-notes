a23c6adf3aa68b8aa4279aef2f102e400a0145be
Dmitry Barskov <barskoidim@gmail.com>
2020-04-01 10:22:28 +0300

    update specs, notify when note created

--- app/interactors/subscriptions/notify.rb
+++ app/interactors/subscriptions/notify.rb
@@ -6,7 +6,9 @@ module Subscriptions
 
     def call
       note.user.followers.each do |follower|
-        NotifySingle.call(follower: follower.follower, note: note)
+        SubscriptionMailer.note_notification_email(
+          follower: follower.follower, note: note
+        ).deliver_later
       end
     end
   end
--- spec/interactors/subscriptions/notify_spec.rb
+++ spec/interactors/subscriptions/notify_spec.rb
@@ -5,20 +5,28 @@ describe Subscriptions::Notify do
     let(:user) { create(:user) }
     let(:note) { create(:note, user: user) }
     let(:followers) { create_list(:user, 3) }
+    let(:fake_delivery) do
+      instance_double("ActionMailer::MessageDelivery")
+    end
 
     before do
       followers.each do |follower|
         Subscriptions::Create.call(followee: user, follower: follower)
       end
-      allow(Subscriptions::NotifySingle).to receive(:call)
+      allow(SubscriptionMailer).to(
+        receive(:note_notification_email).and_return(fake_delivery)
+      )
+      allow(fake_delivery).to receive(:deliver_later)
     end
 
     it "sends emails to followers" do
       followers.each do |follower|
-        expect(Subscriptions::NotifySingle).to(
-          receive(:call).with(note: note, follower: follower)
+        expect(SubscriptionMailer).to(
+          receive(:note_notification_email)
+            .with(note: note, follower: follower)
         )
       end
+      expect(fake_delivery).to receive(:deliver_later)
       described_class.call(note: note)
     end
   end
