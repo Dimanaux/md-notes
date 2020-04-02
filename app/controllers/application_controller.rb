ef38059c63024b138933a65c6ba35fbb8062e18e
Dmitry Barskov <barskovdmitry42@gmail.com>
2020-04-02 19:33:06 +0300

    Update app/controllers/application_controller.rb
    Co-Authored-By: Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require "application_responder"
 
 class ApplicationController < ActionController::Base
-  self.responder = ApplicationResponder
+  responders :flash
   respond_to :html
 
   include Authentication
