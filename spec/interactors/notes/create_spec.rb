877dc8bd1795bcd8d4570b3c8529fbf739aaef11
Dmitry Barskov <barskoidim@gmail.com>
2020-04-02 21:14:58 +0300

    refactor spec for organizers

--- spec/interactors/notes/create_spec.rb
+++ spec/interactors/notes/create_spec.rb
@@ -1,15 +1,7 @@
 require "rails_helper"
 
 describe Notes::Create do
-  describe ".organized" do
-    subject { described_class.organized }
-
-    let(:expected_interactors) do
-      [
-        Notes::UpdateSlug
-      ]
-    end
-
-    it { is_expected.to eq(expected_interactors) }
-  end
+  include_examples "organized with", [
+    Notes::UpdateSlug
+  ]
 end
