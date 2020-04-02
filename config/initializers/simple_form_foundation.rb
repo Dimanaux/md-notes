c55d21b7ee4415ad24c5e758ef0c14981a367548
Dmitry Barskov <barskoidim@gmail.com>
2020-04-02 12:44:01 +0300

    update rubocop config

--- config/initializers/simple_form_foundation.rb
+++ config/initializers/simple_form_foundation.rb
@@ -29,7 +29,7 @@ SimpleForm.setup do |config|
     # b.use :hint,  wrap_with: { tag: :span, class: :hint }
   end
 
-  config.wrappers :horizontal_form, tag: 'div', class: 'row', hint_class: :field_with_hint, error_class: :error, valid_class: :valid do |b|
+  config.wrappers :horizontal_form, tag: "div", class: "row", hint_class: :field_with_hint, error_class: :error, valid_class: :valid do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -38,23 +38,23 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
 
-    b.wrapper :label_wrapper, tag: :div, class: 'small-3 columns' do |ba|
-      ba.use :label, class: 'text-right inline'
+    b.wrapper :label_wrapper, tag: :div, class: "small-3 columns" do |ba|
+      ba.use :label, class: "text-right inline"
     end
 
-    b.wrapper :right_input_wrapper, tag: :div, class: 'small-9 columns' do |ba|
+    b.wrapper :right_input_wrapper, tag: :div, class: "small-9 columns" do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: :small, class: :error }
       # ba.use :hint,  wrap_with: { tag: :span, class: :hint }
     end
   end
 
-  config.wrappers :horizontal_radio_and_checkboxes, tag: 'div', class: 'row' do |b|
+  config.wrappers :horizontal_radio_and_checkboxes, tag: "div", class: "row" do |b|
     b.use :html5
     b.optional :readonly
 
-    b.wrapper :container_wrapper, tag: 'div', class: 'small-offset-3 small-9 columns' do |ba|
-      ba.wrapper tag: 'label', class: 'checkbox' do |bb|
+    b.wrapper :container_wrapper, tag: "div", class: "small-offset-3 small-9 columns" do |ba|
+      ba.wrapper tag: "label", class: "checkbox" do |bb|
         bb.use :input
         bb.use :label_text
       end
@@ -71,7 +71,7 @@ SimpleForm.setup do |config|
   # Note that you need to adapt this wrapper to your needs. If you need a 4
   # columns form then change the wrapper class to 'small-3', if you need
   # only two use 'small-6' and so on.
-  config.wrappers :inline_form, tag: 'div', class: 'column small-4', hint_class: :field_with_hint, error_class: :error, valid_class: :valid do |b|
+  config.wrappers :inline_form, tag: "div", class: "column small-4", hint_class: :field_with_hint, error_class: :error, valid_class: :valid do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -80,7 +80,7 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
 
-    b.use :label, class: 'hidden-for-small-up'
+    b.use :label, class: "hidden-for-small-up"
     b.use :input
 
     b.use :error, wrap_with: { tag: :small, class: :error }
@@ -90,7 +90,7 @@ SimpleForm.setup do |config|
   # Examples of use:
   # - wrapper_html: {class: 'row'}, custom_wrapper_html: {class: 'column small-12'}
   # - custom_wrapper_html: {class: 'column small-3 end'}
-  config.wrappers :customizable_wrapper, tag: 'div', error_class: :error, valid_class: :valid do |b|
+  config.wrappers :customizable_wrapper, tag: "div", error_class: :error, valid_class: :valid do |b|
     b.use :html5
     b.optional :readonly
 
@@ -103,7 +103,7 @@ SimpleForm.setup do |config|
   end
 
   # CSS class for buttons
-  config.button_class = 'button'
+  config.button_class = "button"
 
   # Set this to div to make the checkbox and radio properly work
   # otherwise simple_form adds a label tag instead of a div around
@@ -111,7 +111,7 @@ SimpleForm.setup do |config|
   config.item_wrapper_tag = :div
 
   # CSS class to add for error notification helper.
-  config.error_notification_class = 'alert-box alert'
+  config.error_notification_class = "alert-box alert"
 
   # The default wrapper to be used by the FormBuilder.
   config.default_wrapper = :vertical_form
--- spec/features/user/account/cancel_spec.rb
+++ spec/features/user/account/cancel_spec.rb
@@ -10,7 +10,9 @@ describe "Cancel Account" do
     click_link "Cancel my account"
 
     expect(page).to have_content("Sign in")
-    expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
+    expect(page).to have_content(
+      "Bye! Your account has been successfully cancelled. We hope to see you again soon."
+    )
 
     click_link "Sign in"
     fill_form(:user, current_user.attributes.slice(:email, :password))
