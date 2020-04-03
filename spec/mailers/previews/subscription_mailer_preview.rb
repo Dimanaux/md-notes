65ddc8e545e4b9571edc5b1f60739ea24256bb3f
Dmitry Barskov <barskoidim@gmail.com>
2020-04-03 22:42:01 +0300

    add previews for mailers

--- spec/mailers/previews/subscription_mailer_preview.rb
+++ spec/mailers/previews/subscription_mailer_preview.rb
@@ -1,3 +1,9 @@
-# Preview all emails at http://localhost:3000/rails/mailers/subscription_mailer
+# /rails/mailers/subscription_mailer
 class SubscriptionMailerPreview < ActionMailer::Preview
+  def note_notification_email
+    SubscriptionMailer.note_notification_email(
+      follower: FactoryBot.build(:user),
+      note: FactoryBot.build(:note)
+    )
+  end
 end
