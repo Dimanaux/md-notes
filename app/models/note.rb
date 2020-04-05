eeef1ffe44639d121e9446abe24b0d4c30e94b33
Dmitry Barskov <barskoidim@gmail.com>
2020-04-05 17:08:15 +0300

    remove search by author username

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,15 +1,11 @@
 class Note < ApplicationRecord
   include PgSearch::Model
-  multisearchable against: %i[title content author_username]
+  multisearchable against: %i[title content]
 
   belongs_to :author, class_name: "User"
 
   validates :slug, :title, presence: true, uniqueness: { scope: :author_id }
   validates :content, presence: true
-
-  def auhtor_username
-    auhtor.username
-  end
 
   def to_param
     slug
