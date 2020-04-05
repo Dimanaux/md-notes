7d851772c0f830bf6cf2c2b6463a5fd6f6ba4883
Dmitry Barskov <barskoidim@gmail.com>
2020-04-05 16:50:27 +0300

    install pg_search

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,15 @@
 class Note < ApplicationRecord
+  include PgSearch::Model
+  multisearchable against: %i[title content author_username]
+
   belongs_to :user
 
   validates :slug, :title, presence: true, uniqueness: { scope: :user_id }
   validates :content, presence: true
+
+  def auhtor_username
+    auhtor.username
+  end
 
   def to_param
     slug
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200302122144) do
+ActiveRecord::Schema.define(version: 2020_04_05_134548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,12 +18,31 @@ ActiveRecord::Schema.define(version: 20200302122144) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.bigint "user_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.index ["slug", "user_id"], name: "index_notes_on_slug_and_user_id", unique: true
-    t.index ["user_id"], name: "index_notes_on_user_id"
+    t.index ["author_id"], name: "index_notes_on_author_id"
+    t.index ["slug", "author_id"], name: "index_notes_on_slug_and_author_id", unique: true
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "followee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followee_id", "follower_id"], name: "index_subscriptions_on_followee_id_and_follower_id", unique: true
+    t.index ["followee_id"], name: "index_subscriptions_on_followee_id"
+    t.index ["follower_id"], name: "index_subscriptions_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,5 +73,7 @@ ActiveRecord::Schema.define(version: 20200302122144) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "notes", "users"
+  add_foreign_key "notes", "users", column: "author_id"
+  add_foreign_key "subscriptions", "users", column: "followee_id"
+  add_foreign_key "subscriptions", "users", column: "follower_id"
 end
