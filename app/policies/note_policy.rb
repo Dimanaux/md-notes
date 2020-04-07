4e4975ef7ed2dd17c4ea404afc1e58c6039fb4f7
Dmitry Barskov <barskoidim@gmail.com>
2020-04-07 23:44:38 +0300

    note policy

---
+++ app/policies/note_policy.rb
@@ -0,0 +1,35 @@
+class NotePolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    author?
+  end
+
+  def destroy?
+    author?
+  end
+
+  def author?
+    user.present? && note.author == user
+  end
+
+  def note
+    record
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
