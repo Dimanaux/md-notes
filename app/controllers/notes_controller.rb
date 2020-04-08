b78a338a1c791ec2778e1f8744581fd0ff277f5a
Dmitry Barskov <barskoidim@gmail.com>
2020-04-08 21:29:17 +0300

    support pagination in notes_controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,13 @@
 class NotesController < ApplicationController
-  expose :notes, -> { Note.all }
+  expose :notes, :fetch_notes
 
   def index
     render "users/notes/index"
+  end
+
+  private
+
+  def fetch_notes
+    Note.order(:created_at).page params[:page]
   end
 end
