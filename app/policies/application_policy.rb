31bc8bf11d65d735b859deb82161b6ab7b59efa1
Dmitry Barskov <barskoidim@gmail.com>
2020-04-10 19:49:11 +0300

    add fallback implementation for all actions in application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,2 +1,29 @@
 class ApplicationPolicy < ActionPolicy::Base
+  def index?
+   false
+ end
+
+  def show?
+   false
+ end
+
+  def create?
+   false
+ end
+
+  def new?
+   create?
+ end
+
+  def update?
+   false
+ end
+
+  def edit?
+   update?
+ end
+
+  def destroy?
+   false
+  end
 end
