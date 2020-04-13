58462e20aeb444104dd65b6fa67153958e31e21c
Dmitry Barskov <barskoidim@gmail.com>
2020-04-13 19:45:31 +0300

    add spec for query

---
+++ spec/queries/filtered_notes_query_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe FilteredNotesQuery do
+  subject(:query) { FilteredNotesQuery.new(Note, params) }
+
+  def note(title, content)
+    create(:note, title: title, content: content)
+  end
+
+  describe ".all" do
+    subject { query.all }
+
+    let(:params) { { query: "one" } }
+    let!(:notes) do
+      {
+        one: note("one", "two"),
+        two: note("two", "one"),
+        three: note("three", "three")
+      }
+    end
+
+    it { is_expected.to include(notes[:one], notes[:two]) }
+
+    it { is_expected.not_to include(notes[:three]) }
+  end
+end
