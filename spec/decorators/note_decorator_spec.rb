e56428c01e4fe1bfc576b150d728dc89863a1fc4
Dmitry Barskov <barskoidim@gmail.com>
2020-04-13 19:58:03 +0300

    add spec for note decorator

---
+++ spec/decorators/note_decorator_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe NoteDecorator do
+  subject(:decorated) { note.decorate }
+
+  describe ".created_at" do
+    let(:note) { build(:note, created_at: Date.new(1970, 01, 01)) }
+
+    it "formats date" do
+      expect(decorated.created_at).to eq("1970.01.01")
+    end
+  end
+end
