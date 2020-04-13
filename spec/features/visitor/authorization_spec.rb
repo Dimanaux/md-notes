1ef318151c24877f62df04ff8d4f3bb80ccfaa50
Dmitry Barskov <barskoidim@gmail.com>
2020-04-13 20:05:20 +0300

    fix code style offence

--- spec/features/visitor/authorization_spec.rb
+++ spec/features/visitor/authorization_spec.rb
@@ -5,7 +5,7 @@ describe "Authorization" do
     note = create(:note)
     visit edit_user_note_path(note.author, note)
 
-    expect(current_path).to eq new_user_session_path
+    expect(page).to have_current_path new_user_session_path
     expect(page).to have_content "You need to sign in or sign up before continuing"
   end
 end
