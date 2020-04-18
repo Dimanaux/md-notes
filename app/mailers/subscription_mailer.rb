7b3582642df9a97be9231a9b900028f1b7d6c924
Dmitry Barskov <barskoidim@gmail.com>
2020-04-18 17:32:00 +0300

    fix mailers links

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,6 +1,8 @@
 class SubscriptionMailer < ApplicationMailer
   default from: "md_notes_notifications@example.com"
 
+  helper :notes
+
   def note_notification_email(params)
     @follower = params[:follower]
     @note = params[:note]
