f85b917e9aa8d943469f3650c204c6f66e165da9
Dmitry Barskov <barskoidim@gmail.com>
2020-04-22 00:44:55 +0300

    lazy loading for pdf and md engines

--- app/decorators/html_note.rb
+++ app/decorators/html_note.rb
@@ -1,15 +1,19 @@
 class HtmlNote < ApplicationDecorator
   delegate_all
 
-  MARKDOWN_ENGINE = Redcarpet::Markdown.new(
-    Redcarpet::Render::HTML, fenced_code_blocks: true
-  )
-
   def title
     "<h1>#{object.title}</h1>"
   end
 
   def content
-    @content ||= MARKDOWN_ENGINE.render(object.content)
+    @content ||= HtmlNote.markdown_engine.render(object.content)
+  end
+
+  private
+
+  def self.markdown_engine
+    @markdown_engine ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML, fenced_code_blocks: true
+    )
   end
 end
--- app/decorators/pdf_note.rb
+++ app/decorators/pdf_note.rb
@@ -1,13 +1,17 @@
 class PdfNote < ApplicationDecorator
   delegate_all
 
-  PDF_ENGINE = WickedPdf.new
-
   def filename
     "#{object.author.username}-#{object.slug}.pdf"
   end
 
   def content
-    PDF_ENGINE.pdf_from_string(title + object.content)
+    PdfNote.pdf_engine.pdf_from_string(title + object.content)
+  end
+
+  private
+
+  def self.pdf_engine
+    @pdf_engine ||= WickedPdf.new
   end
 end
