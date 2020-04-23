56acc3fe9b6df10d342df068412cedfcf04b4999
Dmitry Barskov <barskoidim@gmail.com>
2020-04-23 09:53:37 +0300

    update notes_controller

--- app/controllers/users/notes_controller.rb
+++ app/controllers/users/notes_controller.rb
@@ -15,7 +15,7 @@ module Users
       respond_to do |format|
         format.html { respond_with note.author, note }
         format.pdf do
-          pdf_note = PdfNote.new(HtmlNoteDecorator.new(note))
+          pdf_note = PdfNoteDecorator.new(HtmlNoteDecorator.new(note))
           send_data(pdf_note.content, filename: pdf_note.filename)
         end
       end
